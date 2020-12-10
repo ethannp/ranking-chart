7a362798b2a38a03924b424089065659307841a0
Ethan Phan <ethananhphan@gmail.com>
2020-12-09 23:09:48 -0500

    firebase

---
+++ public/app.js
@@ -0,0 +1,72 @@
+(function () {
+    var firebaseConfig = {
+        apiKey: "AIzaSyC4zbil44wr1l7QjJ8EbKwbTi4h3jxDycc",
+        authDomain: "ranking-chart.firebaseapp.com",
+        databaseURL: "https://ranking-chart-default-rtdb.firebaseio.com",
+        projectId: "ranking-chart",
+        storageBucket: "ranking-chart.appspot.com",
+        messagingSenderId: "649390732902",
+        appId: "1:649390732902:web:93430fc6aa6bd63f499fa7",
+        measurementId: "G-28E2XFZ5CQ"
+    };
+    // Initialize Firebase
+    if (!firebase.apps.length) {
+        firebase.initializeApp(firebaseConfig);
+    }
+
+    //get elements
+    const preObject = document.getElementById('object');
+    //create refs
+    const dbRefObject = firebase.database().ref().child('object');
+
+    //sync object changes
+    dbRefObject.on('value', snap => console.log(snap.val()));
+
+}());
+
+
+var data = [
+    {
+        label: 'NA Score',
+        strokeColor: '#ff6384',
+        data: [
+            {
+                x: new Date('2011-04-11T11:45:00'),
+                y: 25
+            },
+            {
+                x: new Date('2011-04-11T12:51:00'),
+                y: 28
+            },
+            {
+                x: new Date('2011-04-11T14:10:00'),
+                y: 22
+            },
+            {
+                x: new Date('2011-04-11T15:15:00'),
+                y: 18
+            },
+            {
+                x: new Date('2011-04-11T17:00:00'),
+                y: 25
+            },
+            {
+                x: new Date('2011-04-11T21:00:00'),
+                y: 24
+            },
+            {
+                x: new Date('2011-04-12T13:00:00'),
+                y: 24
+            }
+        ]
+    }];
+
+var ctx = document.getElementById('chart').getContext('2d');
+
+var chart = new Chart(ctx).Scatter(data, {
+    bezierCurve: true,
+    scaleShowHorizontalLines: true,
+    scaleShowLabels: true,
+    scaleType: "date",
+    scaleLabel: "<%=value%>"
+});
