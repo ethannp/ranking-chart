5fcc3ac3e4977854224328fc559a8248949ee5f3
Ethan Phan <ethananhphan@gmail.com>
2020-12-10 21:54:10 -0500

    admin

--- public/app.js
+++ public/app.js
@@ -11,7 +11,14 @@ const datetime = document.getElementById('datetime');
 const percent = document.getElementById('percent');
 const submitdata = document.getElementById('submitdata');
 const jptable = document.getElementById('jptable');
-const jpBtn = document.getElementById('updateJP')
+const jpBtn = document.getElementById('updateJP');
+const exportBtn = document.getElementById('export');
+const updTitle = document.getElementById("updatetitle");
+const form = document.getElementById("form");
+const need = document.getElementById("need");
+const datetimeAdmin = document.getElementById('updateDate');
+const datetimeval = document.getElementById('datetimeADMIN');
+const updGraph = document.getElementById('editgraph');
 var minPercent = 0;
 var maxPercent = 1;
 var minDate = 0;
@@ -21,6 +28,8 @@ var datapts = [];
 var jppts = [];
 var jpdatapts = [];
 var data = [];
+var tickcount=0;
+var minval=0;
 
 (function () {
     var firebaseConfig = {
@@ -65,6 +74,14 @@ var data = [];
     db.ref('/info/name').once('value').then((snapshot) => {
         ranking.innerHTML = "Current Ranking: " + snapshot.node_.value_;
     });
+    db.ref('/info/minVal').once('value').then((snapshot) => {
+        minval = snapshot.node_.value_;
+        document.getElementById('min').value = minval;
+    });
+    db.ref('/info/tickCount').once('value').then((snapshot) => {
+        tickcount = snapshot.node_.value_;
+        document.getElementById('tick').value = tickcount;
+    });
 
 
     loginBtn.addEventListener('click', e => {
@@ -72,12 +89,19 @@ var data = [];
         const pass = txtPass.value;
         const auth = firebase.auth();
 
-        const promise = auth.signInWithEmailAndPassword(email, pass);
-        promise.catch(e => e); //console.log(e.message)
+        auth.signInWithEmailAndPassword(email, pass)
+        .then((user) => {
+            document.getElementById('loginStatus').innerHTML ="";
+        })
+        .catch((error) => {
+            document.getElementById('loginStatus').innerHTML = "There was an error when logging in. "+error.message;
+        })
     });
 
     logoutBtn.addEventListener('click', e => {
         firebase.auth().signOut();
+        document.getElementById('enterinfo').innerHTML = "Enter information above then <a onclick=\"createAcc()\"><u>click here to create an account.</u></a>";
+        document.getElementById('loginStatus').innerHTML = "";
     });
 
     // logging in/out
@@ -89,22 +113,41 @@ var data = [];
             txtPass.style.display = "none";
             loginBtn.style.display = "none";
             logoutBtn.style.display = "block";
+            document.getElementById('enterinfo').innerHTML = "";
             infoLogin.innerHTML = "You are logged in as " + user.email.substring(0, user.email.indexOf("@"));
+            db.ref('/verified/'+firebase.auth().currentUser.uid).once('value').then((snapshot) => {
+                if (snapshot.node_.value_) {
+                    form.style.display = "block";
+                    need.style.display = "none";
+                }
+                else{
+                    document.getElementById('loginStatus').innerHTML = "Please wait for me to verify your email. It helps to DM me on discord @fluff#2368";
+                }
+            });
+            
             db.ref('/admins/' + firebase.auth().currentUser.uid).once('value').then((snapshot) => {
                 if (snapshot.node_.value_) {
                     admin.style.display = "block";
                     // jp data 86400000
                     var jpcrown = db.ref('jpcrowndata');
-                    let counter=0;
+                    let counter = 0;
                     jpcrown.on('value', function (snapshot) {
                         snapshot.forEach(function (childSnapshot) {
                             var row = document.createElement("tr");
                             var cell1 = document.createElement("td");
-                            var cell1Text = document.createTextNode(new Date(minDate+ counter*86400000/2).toString().substring(0,21));
+                            var cell1Text = document.createTextNode(new Date(minDate + counter * 86400000 / 2).toString().substring(0, 21));
                             var cell2 = document.createElement("td");
-                            var cell2Text = document.createTextNode(childSnapshot.val().score);
+                            var input = document.createElement("input");
+                            input.id = 'jpedit' + counter;
+                            input.className = "jpedit";
+                            var cell2Text = document.createTextNode("");
+                            input.appendChild(cell2Text);
+                            input.value = childSnapshot.val();
+                            input.type = "number";
+                            input.min = 0;
+                            input.max = 999999;
                             cell1.appendChild(cell1Text);
-                            cell2.appendChild(cell2Text);
+                            cell2.appendChild(input);
                             row.appendChild(cell1);
                             row.appendChild(cell2);
                             counter++;
@@ -120,6 +163,8 @@ var data = [];
             txtPass.style.display = "block";
             infoLogin.innerHTML = "";
             admin.style.display = "none";
+            form.style.display = "none";
+            need.style.display = "block";
         }
     });
 
@@ -145,6 +190,18 @@ var data = [];
     })
 }());
 
+function createAcc(){
+    const email = txtUser.value;
+    const pass = txtPass.value;
+    firebase.auth().createUserWithEmailAndPassword(email, pass)
+    .then((user) =>{
+        //signed in
+    })
+    .catch((error) => {
+        document.getElementById('loginStatus').innerHTML ="There was an error when creating your account. "+error.message;
+    })
+}
+
 function formatPts() {
     pts.forEach(element => {
         datapts.push({
@@ -152,13 +209,18 @@ function formatPts() {
             y: element.score
         })
     });
+    let count = 0;
     jppts.forEach(element => {
         jpdatapts.push({
-            x: new Date(element.date),
-            y: element.score
+            x: new Date(minDate + count * 86400000 / 2),
+            y: element
         })
+        count++;
     });
     datapts.shift();
+    datapts.sort(function(a,b){
+        return new Date(b.x)-new Date(a.x);
+    });
     data = [
         {
             label: 'NA Crown',
@@ -178,20 +240,26 @@ function formatPts() {
             bezierCurve: true,
             scaleShowLabels: true,
             scaleShowHorizontalLines: true,
-            scaleShowVerticalLines: false,
+            scaleShowVerticalLines: true,
+            scaleGridLineWidth: 1,
             scaleOverride: true,
             scaleLabel: "<%=value%>",
-            scaleSteps: 4,
+            scaleArgLabel: "<%=value%>",
+            scaleSteps: tickcount,
             scaleStepWidth: 500,
-            scaleStartValue: 225000,
+            scaleStartValue: minval,
             scaleType: "date",
             useUtc: false,
-            scaleDateTimeFormat: "mmm d | hh:mm",
+            scaleDateTimeFormat: "mmm d | H:MM",
             xScaleOverride: true,
-            xScaleSteps: 14,
+            xScaleSteps: 7,
             xScaleStartValue: minDate,
-            xScaleStepWidth: (maxDate - minDate) / 14,
+            xScaleStepWidth: (maxDate - minDate) / 7,
+            scaleTimeFormat: "mmm d",
+            legendTemplate: "<%for(var i=0;i<datasets.length;i++){%>"+"&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp"+"<span class=\"<%=name.toLowerCase()%>-legend-marker\" style=\"background-color:<%=datasets[i].strokeColor%>\"></span>"+"&nbsp"+"<%=datasets[i].label%><%}%>"
         });
+        var legend = chart.generateLegend();
+        document.getElementById("chart-legend").innerHTML = legend;
 }
 /*
         
@@ -206,18 +274,56 @@ txtPass.addEventListener("keyup", function (event) {
 
 submitdata.addEventListener('click', e => {
     var enteredDate = (new Date(datetime.value)).getTime();
-    var d = new Date();
+    /*var d = new Date();
     var localOffset = d.getTimezoneOffset() * 60000;
-    var pst = (enteredDate + localOffset) + (3600000 * -8);
-    if (score.value.length != 0 && score.checkValidity() && pointsto.value.length != 0 && pointsto.checkValidity() && percent.checkValidity() && percent.value.length != 0 && pst <= maxDate && pst >= minDate) {
+    var pst = (enteredDate + localOffset) + (3600000 * -8);*/
+    if (score.value.length != 0 && score.checkValidity() && pointsto.value.length != 0 && pointsto.checkValidity() && percent.checkValidity() && percent.value.length != 0 && enteredDate <= maxDate && enteredDate >= minDate) {
         const db = firebase.database().ref("/nacrowndata/");
         db.push().set({
-            date: pst,
+            date: enteredDate,
             score: (parseInt(score.value) + parseInt(pointsto.value)),
         })
-        document.getElementById('submitvalid').innerHTML = "Data successfully added."
         score.value = "";
         pointsto.value = "";
         percent.value = "";
+        window.location.reload();
     }
 });
+
+jpBtn.addEventListener('click', e => {
+    const db = firebase.database();
+    for (let i = 0; i < 15; i++) {
+        db.ref('/jpcrowndata/' + i).set(document.getElementById('jpedit' + i).value);
+    }
+    window.location.reload();
+});
+
+exportBtn.addEventListener('click', e=>{
+    var link = document.createElement('a');
+    link.download = ranking.innerHTML.substring(17)+'.png';
+    link.href = document.getElementById('chart').toDataURL()
+    link.click();
+})
+
+updTitle.addEventListener('click', e=>{
+    const db = firebase.database();
+    db.ref('/info/name').set(document.getElementById('title').value);
+    window.location.reload();
+})
+
+datetimeAdmin.addEventListener('click', e=> {
+    const db = firebase.database();
+    var enteredDate = (new Date(datetimeval.value)).getTime();
+    db.ref('/info/minDate').set(enteredDate);
+    db.ref('/info/maxDate').set(enteredDate+(86400000*7));
+    window.location.reload();
+});
+
+updGraph.addEventListener('click', e=>{
+    const db = firebase.database();
+    var minval = document.getElementById('min').value;
+    var tick = document.getElementById('tick').value;
+    db.ref('/info/minVal').set(parseInt(minval));
+    db.ref('/info/tickCount').set(parseInt(tick));
+    window.location.reload();
+})
